4b9363e6e8f81e1381b3001e742760ed476703fa
iakshayrathee <96234377+iakshayrathee@users.noreply.github.com>
2023-08-17 15:42:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ const secret = "asdfe45we45w345wegw345werjktjwertkj";
 dotenv.config();
 const BASE_URL = process.env.BASE_URL;
 
-app.use(cors({ credentials: true, origin: "*"},
-res.setHeader("Access-Control-Allow-Origin", "*")  ));
+app.use(cors({ credentials: true, origin: "*" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
